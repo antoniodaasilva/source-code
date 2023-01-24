bc428085b961e9c69dfc7aed06ea6e998e328f5c
antoniodebrito <antonio11brito@gmail.com>
2023-01-23 22:06:43 -0300

    :sparkles: feat: add new php exercises

---
+++ php/php7/echo_print.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Echo e Print</title>
+  </head>
+
+  <body>
+    <?php 
+      echo 'Com aspas simples<br/>';
+      echo 'Com aspas duplas<br/>';
+      print 'Com aspas simples<br/>';
+      print 'Com aspas duplas<br/>';
+      echo print("retorno")
+    ?>
+  </body>
+
+</html>
