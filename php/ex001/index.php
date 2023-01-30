1ba9edd22cac86eed6e1d2fde2ef72afc71a1f9a
antoniodebrito <antonio11brito@gmail.com>
2023-01-30 20:50:18 -0300

    :sparkles: feat: new exercises

---
+++ php/ex001/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Primeiro código</title>
+</head>
+<body>
+  <?php 
+    echo "Olá, mundo \u{1f30e}";
+    
+  ?>
+</body>
+</html>
+
---
+++ php/ex002/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php 
+    // phpinfo();
+    
+    $nome = 'Antonio';
+    $idade = 23;
+
+    define('SENHA', 'senha_usuario');
+
+    echo $idade;
+    echo '<br />';
+    echo $nome;
+    echo '<br />';
+
+    echo SENHA
+    
+  ?>
+</body>
+</html>
+
