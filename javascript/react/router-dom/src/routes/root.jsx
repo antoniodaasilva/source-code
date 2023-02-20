3a9ff1190699eba8c98971ae078db565c81126be
antoniodebrito <antonio11brito@gmail.com>
2023-02-19 22:31:38 -0300

    laoding data

--- javascript/react/router-dom/src/routes/root.jsx
+++ javascript/react/router-dom/src/routes/root.jsx
@@ -37,14 +37,28 @@ export function Root() {
           </form>
         </div>
         <nav>
-          <ul>
-            <li>
-              <Link to={`/contacts/1`}>Your name</Link>
-            </li>
-            <li>
-              <Link to={`/contacts/2`}>Your name</Link>
-            </li>
-          </ul>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
         </nav>
       </div>
       <div id="detail">
