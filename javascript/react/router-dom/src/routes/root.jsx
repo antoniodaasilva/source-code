838846606b7cb65dc10bc243cdfefc5907c1218e
antoniodebrito <antonio11brito@gmail.com>
2023-02-19 22:29:18 -0300

    routes first part

---
+++ javascript/react/router-dom/src/routes/root.jsx
@@ -0,0 +1,55 @@
+import { Link, Outlet, useLoaderData } from "react-router-dom";
+import { getContacts } from "../contacts"
+
+export async function loader() {
+  const contacts = await getContacts()
+  return { contacts }
+}
+
+export function Root() {
+  const { contacts } = useLoaderData()
+
+  return (
+    <>
+      <div id="sidebar">
+        <h1>React Router Contacts</h1>
+        <div>
+          <form id="search-form" role="search">
+            <input 
+              id="q"
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+            />
+            <div
+              id="search-spinner"
+              aria-hidden
+              hidden={true}
+            />
+            <div
+              className="sr-only"
+              aria-live="polite"
+            ></div>
+          </form>
+          <form action="post">
+            <button type="submit">New</button>
+          </form>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link to={`/contacts/1`}>Your name</Link>
+            </li>
+            <li>
+              <Link to={`/contacts/2`}>Your name</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div id="detail">
+        <Outlet />
+      </div>
+    </>
+  )
+}
