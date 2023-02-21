fb0fef2ce9ebcd32f6f2864a4870bd8160dcf5a4
antoniodebrito <antonio11brito@gmail.com>
2023-02-21 17:39:44 -0300

    :sparkles: feat: create Textarea component

--- js-ts/react/rocketseat/rocket-notes/src/main.jsx
+++ js-ts/react/rocketseat/rocket-notes/src/main.jsx
@@ -3,13 +3,13 @@ import ReactDom from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
-import { Profile } from './pages/Profile'
+import { New } from './pages/New'
 
 ReactDom.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Profile />
+      <New />
     </ThemeProvider>
   </React.StrictMode>
 )
