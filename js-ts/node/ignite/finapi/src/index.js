b3172cc87e9d90551d1388462b88abd528e67193
antoniodebrito <antonio11brito@gmail.com>
2023-02-26 19:51:43 -0300

    :sparkles: feat: cria middleware de verificacao

--- js-ts/node/ignite/finapi/src/index.js
+++ js-ts/node/ignite/finapi/src/index.js
@@ -1,4 +1,4 @@
-const { response } = require("express");
+const { response, request } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid")
 
@@ -6,6 +6,23 @@ const app = express();
 const customers = [];
 
 app.use(express.json());
+
+
+//Middleware
+function verifyIfExistAccountCPF(req, res, next) {
+  const { cpf } = req.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return res.status(400).json({ error: "Customer not found" })
+  }
+
+  req.customer = customer;
+
+  return next();
+
+}
 
 app.post("/account", (req, res) => {
   const { cpf, name } = req.body;
@@ -30,6 +47,14 @@ app.post("/account", (req, res) => {
   return res.status(201).send();
   
 });
+
+// app.use(verifyIfExistAccountCPF)
+
+app.get("/statement", verifyIfExistAccountCPF, (req, res) => {
+  const { customer } = req;
+
+  return res.json(customer.statement)
+})
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`server is running on http://localhost:${PORT}`))
