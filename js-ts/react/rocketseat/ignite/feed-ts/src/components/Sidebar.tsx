bb57983bdb606230a6f8c91f9656bfba4c336799
antoniodebrito <antonio11brito@gmail.com>
2023-03-01 00:49:50 -0300

    :sparkles: #F032 add typescript

---
+++ js-ts/react/rocketseat/ignite/feed-ts/src/components/Sidebar.tsx
@@ -0,0 +1,31 @@
+import { PencilLine } from "phosphor-react";
+
+import styles from './Sidebar.module.css'
+
+import capa from '../assets/capa.png'
+import { Avatar } from "./Avatar";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover}
+        src={capa}
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/antoniodebrito.png" />
+
+        <strong>Antonio Brito</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
