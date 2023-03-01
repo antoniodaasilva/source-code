b82689f85da6c73a51ebf987724a5d913538d92b
antoniodebrito <antonio11brito@gmail.com>
2023-02-28 22:35:48 -0300

    :sparkles: #F024: use key property

--- js-ts/react/rocketseat/ignite/feed/src/App.jsx
+++ js-ts/react/rocketseat/ignite/feed/src/App.jsx
@@ -20,6 +20,20 @@ const posts = [
     ],
     publishedAt: new Date('2022-05-03 20:00:00')
   },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Antonio Brito',
+      role: 'Frontend @Devsfree'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+  },
 ]
 
 export function App() {
@@ -33,13 +47,15 @@ export function App() {
         <main>
           {
             posts.map(post => {
-              return (<Post 
-              
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
+              return (
+                <Post 
+                  key={post.id}
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
 
-              />)
+                />
+              )
             })
           }
         </main>
