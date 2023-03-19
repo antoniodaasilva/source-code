20edeca2120c16e503f1901803d7e1b058a51f87
antoniodebrito <antonio11brito@gmail.com>
2023-03-19 17:06:33 -0300

    :sparkles: feat: codigo review

--- js-ts/react/rocketseat/ignite/timer/src/App.tsx
+++ js-ts/react/rocketseat/ignite/timer/src/App.tsx
@@ -1,17 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components"
-import { Button } from "./components/Button"
+import { Router } from "./Router"
 import { GlobalStyle } from "./styles/global"
 import { defaultTheme } from "./styles/themes/default"
 
 export function App() { return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-      <Button  variant="primary"/>
-      <Button  variant="secondary"/>
-      <Button  variant="success"/>
-      <Button  variant="danger"/>
-
-    </ThemeProvider>
-  )
+  <ThemeProvider theme={defaultTheme}>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+    <GlobalStyle />
+  </ThemeProvider>
+);
 }
 
