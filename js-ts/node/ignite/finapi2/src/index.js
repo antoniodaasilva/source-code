89d4ebdaf9ac409ae3a091ad05da5d80e65de235
antoniodebrito <antonio11brito@gmail.com>
2023-04-16 22:46:00 -0300

    class #F016

--- js-ts/node/ignite/finapi2/src/index.js
+++ js-ts/node/ignite/finapi2/src/index.js
@@ -8,6 +8,21 @@ const PORT = 3456;
 
 const customers = [];
 
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+
+  if (!customer) {
+    return res.status(400).json({ error: "Customer not found" })
+  }
+
+  req.customer = customer
+
+  return next();
+}
+
 app.post("/account", (req, res) => {
   const { cpf, name } = req.body;
 
@@ -16,17 +31,28 @@ app.post("/account", (req, res) => {
   );
 
   if (customersAlreadyExists) {
-    return res.status(400).json({ error: "Customer alreay exists!" });
+    return res.status(400).json({ error: "Customer already exists!" });
   }
+
+  const id = uuidv4();
 
   customers.push({
     cpf,
     name,
-    id: uuidv4(),
+    id,
     statement: [],
   });
 
   return res.status(201).send();
+});
+
+
+
+
+app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = request;
+
+  return res.json(customer.statement);
 });
 
 app.listen(
