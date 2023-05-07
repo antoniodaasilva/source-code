99fceae6c1af24d9916465ef0fa3911b31a8679f
antoniodebrito <antonio11brito@gmail.com>
2023-05-06 23:36:13 -0300

    :tada: create base project

--- js-ts/react/rocketseat/ignite/timer/src/App.tsx
+++ js-ts/react/rocketseat/ignite/timer/src/App.tsx
@@ -1,16 +1,11 @@
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "styled-components"
-import { Router } from "./Router"
-import { GlobalStyle } from "./styles/global"
-import { defaultTheme } from "./styles/themes/default"
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyles } from "./styles/global";
 
-export function App() { return (
-  <ThemeProvider theme={defaultTheme}>
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
-    <GlobalStyle />
-  </ThemeProvider>
-);
+export function App() {
+  return (
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+      </ThemeProvider>
+    )
 }
-
