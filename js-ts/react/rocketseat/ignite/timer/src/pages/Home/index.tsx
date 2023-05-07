e41f43b8e916c320afa9dba83bacc07db179c667
antoniodebrito <antonio11brito@gmail.com>
2023-05-07 15:51:53 -0300

    :construction: #F044 home

--- js-ts/react/rocketseat/ignite/timer/src/pages/Home/index.tsx
+++ js-ts/react/rocketseat/ignite/timer/src/pages/Home/index.tsx
@@ -1,9 +1,31 @@
-import { HomeContainer } from "./styles";
+import { Play } from "phosphor-react";
+import { CountdownContainer, FormContainer, HomeContainer } from "./styles";
 
 export function Home() {
   return (
     <HomeContainer>
-      Home
+      <form>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input type="text" id="task" />
+          <label htmlFor="minutesAmount">durante</label>
+          <input type="number" id="minutesAmount" />
+          <span>Minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <button type="submit">
+          Começar
+          <Play size={24}/>
+        </button>
+      </form>
     </HomeContainer>
-  )
+  );
 }
