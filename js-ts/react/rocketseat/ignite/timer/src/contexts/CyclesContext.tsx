e4726a289a939f56f457a4faf500845f0f862a71
antoniodebrito <antonio11brito@gmail.com>
2023-05-14 22:33:19 -0300

    :construction: feat: #F075 utilizando immer

--- js-ts/react/rocketseat/ignite/timer/src/contexts/CyclesContext.tsx
+++ js-ts/react/rocketseat/ignite/timer/src/contexts/CyclesContext.tsx
@@ -1,6 +1,7 @@
 import { ReactNode, createContext, useReducer, useState } from "react";
 import { Cycle, cyclesReducer } from "../reducers/cycles/reducer";
-import { ActionTypes, addNewCycleAction, interruptedCurrentCycleAction, markCurrentCycleAsFinisehdAction } from "../reducers/cycles/actions";
+import {addNewCycleAction, interruptedCurrentCycleAction, markCurrentCycleAsFinisehdAction } from "../reducers/cycles/actions";
+
 
 interface CreateCycleData {
   task: string;
