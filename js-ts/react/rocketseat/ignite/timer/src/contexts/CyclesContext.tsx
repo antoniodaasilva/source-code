9adc6983a32f9b5baf2593035aeda97d7a29a265
antoniodebrito <antonio11brito@gmail.com>
2023-05-14 22:17:20 -0300

    :construction: feat: #F074 separando actions

--- js-ts/react/rocketseat/ignite/timer/src/contexts/CyclesContext.tsx
+++ js-ts/react/rocketseat/ignite/timer/src/contexts/CyclesContext.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, createContext, useReducer, useState } from "react";
-import { ActionTypes, Cycle, cyclesReducer } from "../reducers/cycles";
+import { Cycle, cyclesReducer } from "../reducers/cycles/reducer";
+import { ActionTypes, addNewCycleAction, interruptedCurrentCycleAction, markCurrentCycleAsFinisehdAction } from "../reducers/cycles/actions";
 
 interface CreateCycleData {
   task: string;
@@ -42,12 +43,7 @@ export function CyclesContextProvider({
   }
 
   function markCurrentCycleAsFinished() {
-    dispatch({
-      type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
-      payload: {
-        activeCycleId,
-      },
-    });
+    dispatch(markCurrentCycleAsFinisehdAction());
   }
 
   function createNewCycle(data: CreateCycleData) {
@@ -60,23 +56,13 @@ export function CyclesContextProvider({
       startDate: new Date(),
     };
 
-    dispatch({
-      type: ActionTypes.ADD_NEW_CYCLE,
-      payload: {
-        newCycle,
-      },
-    });
+    dispatch(addNewCycleAction(newCycle));
 
     setAmountSecondsPassed(0);
   }
 
   function interruptCurrentCycle() {
-    dispatch({
-      type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
-      payload: {
-        activeCycleId,
-      },
-    });
+    dispatch(interruptedCurrentCycleAction());
   }
 
   return (
