7984402914e4c6a96f2b59fa803496395a0c673b
antoniodebrito <antonio11brito@gmail.com>
2023-05-16 19:36:34 -0300

    :sparkles: #F080 componente Header

--- js-ts/react/rocketseat/ignite/dt-money/dt-money-yarn/src/App.tsx
+++ js-ts/react/rocketseat/ignite/dt-money/dt-money-yarn/src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import { Transactions } from "./pages/Transactions";
 
 export function App() {
-  return <ThemeProvider theme={defaultTheme}>
-    <h1>Hello</h1>
-
-    <GlobalStyle />
-  </ThemeProvider>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Transactions />
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
---
+++ js-ts/react/rocketseat/ignite/dt-money/dt-money-yarn/src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+
+import logoimg from '../../assets/logo.svg'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoimg} alt="" />
+
+        <NewTransactionButton>Nova transação</NewTransactionButton>
+      </HeaderContent>
+    </HeaderContainer>
+  )
+}
