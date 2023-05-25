2055fadc6f8d834812323c276c6afcd4be64d786
antoniodebrito <antonio11brito@gmail.com>
2023-05-25 07:11:53 -0300

    :sparkles: Feat: #F112 imagens no Next.js

--- js-ts/react/rocketseat/ignite/shop-01/src/pages/_app.tsx
+++ js-ts/react/rocketseat/ignite/shop-01/src/pages/_app.tsx
@@ -1,15 +1,17 @@
-import { AppProps } from "next/app";
+import { Container, Header } from "../styles/pages/app";
 import { globalStyles } from "../styles/global";
+import { AppProps } from "next/app";
+import logo from "../assets/logo.svg";
+import Image from "next/image";
+
+
 
 globalStyles()
-import logo from "../assets/logo.svg";
-import { Container, Header } from "../styles/pages/app";
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logo.src} alt="" />
+        <Image src={logo} alt="" />
       </Header>
       <Component {...pageProps} />
     </Container>
