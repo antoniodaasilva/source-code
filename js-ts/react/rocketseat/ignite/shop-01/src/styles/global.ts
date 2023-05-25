270f45680fbb960916016fb5b217a35fde9dd327
antoniodebrito <antonio11brito@gmail.com>
2023-05-25 06:48:53 -0300

    :sparkles: Feat: #F110 estilos globais

---
+++ js-ts/react/rocketseat/ignite/shop-01/src/styles/global.ts
@@ -0,0 +1,18 @@
+import { globalCss } from ".";
+
+export const globalStyles = globalCss({
+  "*": {
+    margin: 0,
+    padding: 0,
+  },
+
+  body: {
+    background: '$gray900',
+    color: '$gray100'
+  },
+
+  'body, input, textarea, button': {
+    fontFamily: 'Roboto',
+    fontWeight: 400
+  }
+})
