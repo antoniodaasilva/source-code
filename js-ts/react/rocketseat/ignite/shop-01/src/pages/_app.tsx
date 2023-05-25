eae6fbbbf7e82d65b247a85bcfe30adfe2d5d6af
antoniodebrito <antonio11brito@gmail.com>
2023-05-25 06:58:22 -0300

    :sparkles: Feat: #F111 cabeçalho da aplicação

--- js-ts/react/rocketseat/ignite/shop-01/src/pages/_app.tsx
+++ js-ts/react/rocketseat/ignite/shop-01/src/pages/_app.tsx
@@ -2,7 +2,16 @@ import { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
 
 globalStyles()
+import logo from "../assets/logo.svg";
+import { Container, Header } from "../styles/pages/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logo.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
