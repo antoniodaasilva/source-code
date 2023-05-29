1a7476fef3ce207f6ba26234ab70e3e056a35870
antoniodebrito <antonio11brito@gmail.com>
2023-05-29 19:23:53 -0300

    :sparkles: Feat: new exercises

---
+++ js-ts/react/rocketseat/ignite/layouts/dt-money/src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+
+import logoimg from '../../assets/logo.svg'
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionModal } from "../NewTransactionModal";
+
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoimg} alt="" />
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
+  );
+}
